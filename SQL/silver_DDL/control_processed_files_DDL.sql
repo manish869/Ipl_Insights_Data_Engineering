f1706f49dd3da776406e01d46c98f6f611081f52
manish869 <maurym869@gmail.com>
2026-06-27 00:51:50 +0530

    minor typo correction in silver.control_processed_files DDL

--- SQL/silver_DDL/control_processed_files_DDL.sql
+++ SQL/silver_DDL/control_processed_files_DDL.sql
@@ -1,9 +1,9 @@
 USE [ipl_insights];
 
-IF OBJECT_ID('silver.contol_processed_files') IS NOT NULL
-	DROP TABLE silver.contol_processed_files;
+IF OBJECT_ID('silver.control_processed_files') IS NOT NULL
+	DROP TABLE silver.control_processed_files;
 
-CREATE TABLE silver.contol_processed_files(
+CREATE TABLE silver.control_processed_files(
 file_name NVARCHAR(100) NOT NULL,
 file_path NVARCHAR(1000) NOT NULL,
 file_size INT NOT NULL,
